437ee0e1f16aa5f38a4e8b329452a07a1fc65969
Jack <selentsova12@gmail.com>
2023-11-01 13:17:07 +0200

    HW1

---
+++ HW1.js
@@ -0,0 +1,57 @@
+//1) Create a variable with the name of our planet. 
+var Suturn; 
+
+//2) Create a variable to store the name of the current site visitor.
+var name = "Tanya";
+//3) Create "var" variable named myAge and set its value equal to 30
+var myAge = 30;
+console.log(myAge);
+
+//4) Create "let" variable named myName and set its value equal to any name
+let myName1 = "Katya";
+console.log (myName);
+
+//5) Create "const" variable named myBool and set its value equal to false
+const myBool = false;
+
+
+//6) Display the sum of 15 + 30, using two variables: x and y.
+var x = 15;
+var y = 30;
+console.log(x+y)
+
+//7) Create variable named myUndefined and set its value equal to undefined
+var myUndefined = undefined;
+console.log(myUndefined);
+
+//8) Create variable named dividedNumber and set its value equal myAge divided by 2
+// * NOTE: you should use myAge variable here
+var myAge = 30;
+var dividedNumber = myAge / 2;
+console.log(dividedNumber);
+
+//9) Create variable named multipleNumber and set its value equal myAge multiply by 2
+var myAge = 30;
+var multipleNumber = myAge * 2;
+console.log(multipleNumber);
+
+//10) Create variable named mySentence and set its value to be myName, then goes space and word is, then space and myAge
+//NOTE: Result should be something like "Michael is 30"
+let myName = "Tanya";
+let myAge = 30; 
+let  mySentence = myName +" " + "is"  + " " + myAge;
+console.log(mySentence);
+
+//11) Create variable named myUndefined2 and set its value equal to undefined without "undefined" word usage
+var myUndefined2;
+console.log(myUndefined2);
+
+//12) Declare two variables: admin and name. Assign the value "Bill Gates" to the variable name.Copy the value from the variable name to admin.
+//Print the value of the admin variable.
+
+
+let name1 = "Bill Gates";
+let admin = "";
+admin = name1;
+console.log(admin);
+
